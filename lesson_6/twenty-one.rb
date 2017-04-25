5b3d940b60753c8ad9df7c7ce84524f329433eb9
Jeff Mignone <jeff.mignone@gmail.com>
2017-04-24 22:57:02 -0400

    twenty-one wip

--- lesson_6/twenty-one.rb
+++ lesson_6/twenty-one.rb
@@ -18,20 +18,6 @@ end
 def total(player_or_dealer)
   total = 0
 
-
-
-# sorted = player_or_dealer.sort_by do |k, v|
-#       if v[0] == 'A'
-#         1
-#       else
-#         -1
-#       end
-#     end
-
-# sorted.each do |k, v|
-#   if 
-# end
-
 player_or_dealer.each do |_, v|
   card_val = v[0]
   if card_val == 'A'
@@ -53,18 +39,30 @@ def calculate_ace(total)
   ace
 end
 
-def pick_card(starter_deck)
+def remove_cards(deck)
+  deck.delete_if { |card| card[1].empty? }
+end
 
-  # deck.each do |card|
-  #   if card[1].empty?
-  #     deck.delete(deck.index(card))
-  #   end
-  # end
+def pick_card(deck)
+  remove_cards(deck)
 
-  card = starter_deck.sample
+  card = deck.sample
   number = card[0]
-  suite = starter_deck[starter_deck.index(card)][1].delete(card[1].sample)
+  suite = deck[deck.index(card)][1].delete(card[1].sample)
+  
   [number, suite]
+end
+
+
+def assign_cards(user, deck)
+  index = 1
+  loop do
+    if user[index].nil?
+      user[index] = pick_card(deck)
+      break
+    end
+  index += 1
+  end
 end
 
 def busted?(cards)
@@ -80,31 +78,37 @@ end
 
 
 loop do
+  play_again = ''
   players_cards = {}
   dealers_cards = {}
   player_index = 1
   dealer_index = 1
-  starter_deck = init_deck
-  players_cards["Card #{player_index}"] = pick_card(starter_deck)
-  dealers_cards["Card #{dealer_index}"] = pick_card(starter_deck)
-  dealers_cards["Card #{dealer_index += 1}"] = pick_card(starter_deck)
+  deck = init_deck
+  assign_cards(players_cards, deck)
+  assign_cards(dealers_cards, deck)
+  assign_cards(dealers_cards, deck)
   loop do
-    update_screen(players_cards, dealers_cards, starter_deck)
+    update_screen(players_cards, dealers_cards, deck)
     answer = gets.chomp
 
-    if answer == 'hit'
-      players_cards["Card #{player_index += 1}"] = pick_card(starter_deck)
-    end
+    players_cards["Card #{player_index += 1}"] = pick_card(deck, player) if answer == 'hit'
     
     if answer == 'stay'
       puts "Player stays with a total of #{total(players_cards)}"
+      until total(dealers_cards) >= 17 do
+        dealers_cards["Card #{dealer_index += 1}"] = pick_card(deck)
+      end
+      puts "Dealer busted with #{total(dealers_cards)}! Player wins!"
+      update_screen(players_cards, dealers_cards, deck)
     elsif busted?(players_cards)
+      update_screen(players_cards, dealers_cards, deck)
       puts "Player busted with #{total(players_cards)}! Dealer wins!"
       puts "Play another game? (y/n)"
       play_again = gets.chomp.downcase
       break if play_again.start_with?('y')
+      break if play_again.start_with?('n')
     end   
   end
-
+  break if play_again.start_with?('n')
 end
-puts "player busted"
+
